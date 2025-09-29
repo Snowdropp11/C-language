15a91e8076f4dc4603928f8244aacbccc8a737eb
Yusuf Ilhan <ysf_lhn@yahoo.com>
2025-09-29 11:40:11 +0200

    Proje dosyaları eklendi

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+int main() { //calsitirabilir codun yazildigi kisim
+    printf("Merhaba Dünya!\n");
+    printf("C language dersine basladim")
+
+getch();
+
+    return 0;
+
+
+
+
+}
